4809cbb6dbe7fc9952d34b6a3787eec64f77f8ff
Virat Kamble <mrviratkamble98@gmail.com>
2021-04-17 14:18:02 +0530

    Virat-Added Home page banner

--- index.php
+++ index.php
@@ -10,13 +10,52 @@
     integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" 
     crossorigin="anonymous">
 
+    <!-- styles.css -->
+    <link rel="stylesheet" href="css/styles.css">
+
     <title>Home</title>
 </head>
 <body>
+    <!-- Navigation Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light border-bottom border-primary mb-0" style="background-color: #e3f2fd;">
+        <a class="navbar-brand" href="#">Diapsi</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <?php
-    echo "Hello World";
-    ?>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Login <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Sign Up <span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Banner -->
+    <div id="Banner">
+        <div id="Banner-content" class="border border-primary">
+                <h1>Learn on your schedule Study any topic, anytime.</h1>
+                <strong>Explore thousands of courses starting at ₹455 each.</strong>
+        </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
 
 
 
